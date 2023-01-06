d747ad505f8da8685b7f6a1d484d1ac47ee39161
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-06 23:20:52 +0200

    add menu content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/logo1.png";
 
 function Navbar() {
   return (
     <div>
-      <h1 className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></h1>
+      <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+        <div className="">
+          <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+        </div>
+        {/* Menu */}
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      {/* Hamburger */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+      {/* Mobile Menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+      {/* Social Icons */}
+      <div></div>
     </div>
   );
 }
