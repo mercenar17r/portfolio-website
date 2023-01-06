955ab37aa9688a2efd8417eb00943e902c35aef4
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-06 23:51:21 +0200

    add mobile nav handler and add functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/logo1.png";
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+  const handlerClick = () => setNav(!nav);
   return (
     <div>
       <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
@@ -19,11 +21,17 @@ function Navbar() {
           <li>Contact</li>
         </ul>
         {/* Hamburger */}
-        <div className="md:hidden">
-          <FaBars />
+        <div onClick={handlerClick} className="md:hidden z-10">
+          {!nav ? <FaBars /> : <FaTimes />}
         </div>
         {/* Mobile Menu */}
-        <ul className=" hidden absolute top-0 left-0 w-full h h-screen bg-[#0a192f] flex flex-col justify-center items-center ">
+        <ul
+          className={
+            !nav
+              ? "hidden"
+              : "absolute top-0 left-0 w-full  h-screen bg-[#0a192f] flex flex-col justify-center items-center "
+          }
+        >
           <li className="py-6 text-4xl">Home</li>
           <li className="py-6 text-4xl">About</li>
           <li className="py-6 text-4xl">Skills</li>
