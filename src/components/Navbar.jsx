d119c28b149bea079ce5f3d8503dcd0d1f5ccd7e
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-06 22:55:53 +0200

    add tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h1 className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></h1>
+    </div>
+  );
+}
+
+export default Navbar;
