1ba41bbe36a5ac8486bcd27e2fe169cec8c9a45c
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-07 09:48:22 +0200

    add nice hover animation to icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,9 +49,9 @@ function Navbar() {
         {/* Social Icons */}
         <div className="flex fixed flex-col top-[35%] left-0">
           <ul>
-            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] ">
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px]  hover:ml-[-10px] duration-300  bg-blue-600">
               <a className="flex justify-between items-center w-full text-gray-300">
-                Linkedin <FaFacebook size={30} />
+                Linkedin <FaLinkedin size={30} />
               </a>
             </li>
           </ul>
