dc8d823c720d70b2097fb0bd87a2ac5a7501ec9c
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-07 21:50:31 +0200

    structure container and add grid

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className="w-fu;; h-screen bg-[#0a192f]">
       {/* Container */}
-      <div className="max-w-[700px] mx-auto px-8 flex flex-col justify-center h-full">
+      <div className="max-w-[800px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600">Hi, my name is</p>
         <h1 className="text-4xl sm:text-3xl font-bold text-[#ccd6f6]">
           Cosmin Negrut
