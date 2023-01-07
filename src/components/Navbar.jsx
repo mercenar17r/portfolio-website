3b8220e230c9a217ace519947d84aeb86df2c819
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-07 09:54:46 +0200

    add the rest of icons and change bg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,6 +54,21 @@ function Navbar() {
                 Linkedin <FaLinkedin size={30} />
               </a>
             </li>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px]  hover:ml-[-10px] duration-300  bg-[#333333]">
+              <a className="flex justify-between items-center w-full text-gray-300">
+                GitHub <FaGithub size={30} />
+              </a>
+            </li>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px]  hover:ml-[-10px] duration-300  bg-[#6fb2b0]">
+              <a className="flex justify-between items-center w-full text-gray-300">
+                Email <HiOutlineMail size={30} />
+              </a>
+            </li>
+            <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px]  hover:ml-[-10px] duration-300  bg-[#565f65]">
+              <a className="flex justify-between items-center w-full text-gray-300">
+                CV <BsFillPersonLinesFill size={30} />
+              </a>
+            </li>
           </ul>
         </div>
       </div>
