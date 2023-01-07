00c16a3b8260999690fa7bca6bcc1bed578da1c9
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-07 19:50:17 +0200

    style text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,14 @@ function Hero() {
     <div className="w-fu;; h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p>Hi, my name is</p>
-        <h1>Cosmin Negrut</h1>
-        <h2>I'm a Junior Web Developer.</h2>
-        <p>
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-3xl font-bold text-[#ccd6f6]">
+          Cosmin Negrut
+        </h1>
+        <h2 className="text-4xl sm:text-4xl font-bold text-[#8892b0]">
+          I'm a Junior Web Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
           I'm Junior Web Developer.Currently specializing in building Web
           Applications and constantly learning new technologies.
         </p>
