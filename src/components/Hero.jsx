7fce91dfe3464361c2d93c0837963a74733715f3
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-07 20:27:43 +0200

    add animation to button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,12 @@ function Hero() {
   return (
     <div className="w-fu;; h-screen bg-[#0a192f]">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+      <div className="max-w-[700px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600">Hi, my name is</p>
         <h1 className="text-4xl sm:text-3xl font-bold text-[#ccd6f6]">
           Cosmin Negrut
         </h1>
-        <h2 className="text-4xl sm:text-4xl font-bold text-[#8892b0]">
+        <h2 className="text-4xl sm:text-3xl font-bold text-[#8892b0]">
           I'm a Junior Web Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
@@ -18,11 +18,10 @@ function Hero() {
           Applications and constantly learning new technologies.
         </p>
         <div>
-          |{" "}
-          <button className="text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
             View Work
-            <span>
-              <HiArrowNarrowRight className="ml-3 " />
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
             </span>
           </button>
         </div>
