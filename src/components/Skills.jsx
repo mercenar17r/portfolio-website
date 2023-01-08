f84d9d5710f2efe4eb2cab8dd31d65207b37ea8d
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-08 23:35:10 +0200

    style header

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,11 +11,13 @@ function Skills() {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p>Experience</p>
-          <p>These are the technologies I've worked with</p>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Skills
+          </p>
+          <p className="pi-4">These are the technologies I've worked with</p>
         </div>
         <div>
-          <img src={HTML} alt="HTML icon" />
+          <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
         </div>
       </div>
     </div>
