cef7b0a751006db84f16f9e06ff3142e05caad49
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-08 23:38:27 +0200

    style icons container

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ import GitHub from "../assets/github.png";
 
 function Skills() {
   return (
-    <div name="skills">
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
@@ -16,7 +16,7 @@ function Skills() {
           </p>
           <p className="pi-4">These are the technologies I've worked with</p>
         </div>
-        <div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
           <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
         </div>
       </div>
