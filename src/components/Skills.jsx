20eb51f77eec57febea4925d75110ac1c7fa99d0
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-08 23:26:51 +0200

    add  comp structure

---
+++ src/components/Skills.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import GitHub from "../assets/github.png";
+
+function Skills() {
+  return (
+    <div name="skills">
+      {/* Container */}
+      <div>
+        <div>
+          <p>Experience</p>
+          <p>These are the technologies I've worked with</p>
+        </div>
+        <div>
+          <img src={HTML} alt="HTML icon" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
