50ca1568133942c55ff6c14d23baa33fcd2df605
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-08 23:50:11 +0200

    add hover animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,17 +9,19 @@ function Skills() {
   return (
     <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[900px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
             Skills
           </p>
-          <p className="pi-4">These are the technologies I've worked with</p>
+          <p className="pi-4 mt-10">
+            These are the technologies I've worked with
+          </p>
         </div>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16]">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
-            <p>HTML</p>
+            <p className="my-4">HTML</p>
           </div>
         </div>
       </div>
