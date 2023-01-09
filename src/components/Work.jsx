9b7becd7555ff5aa7ed5382dfa1fd8f519c65821
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-09 17:27:37 +0200

    more on container

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,10 +3,12 @@ import React from "react";
 function Work() {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div>
-          <p>Work</p>
-          <p>//Check out my recent work</p>
+      <div className="max-w-[950px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600 ">
+            Work
+          </p>
+          <p className="py-6">//Check out my recent work</p>
         </div>
         <div>
           <div>
