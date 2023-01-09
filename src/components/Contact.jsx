00b50c17de359e63293d7deb0d758dadd157f64b
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-09 20:24:00 +0200

    style contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,9 +6,11 @@ function Contact() {
       name="contact"
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
-      <form action="">
-        <div>
-          <p>Contact</p>
+      <form action="" className="flex flex-col max-w-[600px] w-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+            Contact
+          </p>
         </div>
       </form>
     </div>
