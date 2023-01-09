75e388a6c83edbc9858fe1bf6a508af853a60c58
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-09 20:33:19 +0200

    add inputs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,6 +16,24 @@ function Contact() {
             call me at number-0730150974
           </p>
         </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
       </form>
     </div>
   );
