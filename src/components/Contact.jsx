421d090bb99a8134b37229f563a8203e444a4750
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-09 21:18:31 +0200

    add method to form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,11 @@ function Contact() {
       name="contact"
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
-      <form action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        method="POST"
+        action="https://getform.io/f/32a14bb5-f8ba-408b-806a-ce29cb7310ca"
+        className="flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Contact
