24813aa30cc679df46cdd8b178f861d0047281ea
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-09 17:21:07 +0200

    style container

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,9 +3,26 @@ import React from "react";
 function Work() {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
-      <div>
-        <p>Work</p>
-        <p>//Check out my recent work</p>
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p>Work</p>
+          <p>//Check out my recent work</p>
+        </div>
+        <div>
+          <div>
+            <div>
+              <div>
+                <span></span>
+              </div>
+              <a href="/">
+                <button></button>
+              </a>
+              <a href="/">
+                <button></button>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
