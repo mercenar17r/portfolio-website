3f513455e3e3e2693f17112d71b3e05f185b6326
Cosmin Negrut <cosminnegrut776@gmail.com>
2023-01-09 20:21:06 +0200

    add contact comp

---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+    >
+      <form action="">
+        <div>
+          <p>Contact</p>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
